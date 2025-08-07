91efa8221a17d59f0506a4ad1c66d07b433b93b2
jaisai1709-afk <jaisai1709@gmail.com>
2025-08-07 17:30:06 +0530

    ai

--- Ai.py
+++ Ai.py
@@ -1,1 +1,27 @@
+import google.generativeai as genai
+
+def startai():
+    genai.configure(api_key="AIzaSyAVMiLgrqz-vSYKqdoDMpj-6IAyiDqlpfQ")
+    global model
+    model = genai.GenerativeModel('gemini-2.5-pro')
+
+
+def chatbot(usr):
+    response = model.generate_content(usr)
+    return response.text
+
+startai()
+
+while True:
+    usr = input("ME : ")
+    res = chatbot(usr)
+    
+    
+    a = open ("Data.txt", "a")
+    a.write()
+
+
+    if usr.lower() == "bye":
+        break
+
 
