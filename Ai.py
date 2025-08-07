6a7318f1be2e5f4cb65608c980e389768ee47693
Indra <indra.rtf.official@gmail.com>
2025-08-07 18:39:18 +0530

    first save for the  project

--- Ai.py
+++ Ai.py
@@ -1,25 +1,30 @@
 import google.generativeai as genai
 
-def startai():
-    genai.configure(api_key="AIzaSyAVMiLgrqz-vSYKqdoDMpj-6IAyiDqlpfQ")
+def startai(npc,api):
+    genai.configure(api_key=api)
     global model
     model = genai.GenerativeModel('gemini-2.5-pro')
 
 
 def chatbot(usr):
     response = model.generate_content(usr)
+    print(response.text)
     return response.text
 
-startai()
+startai("Mahathma Ghandhi","AIzaSyAVMiLgrqz-vSYKqdoDMpj-6IAyiDqlpfQ")
+
+a = open("Data/Data.txt", "a+")
 
 while True:
+    data = a.read()
     usr = input("ME : ")
-    res = chatbot(usr)
-    
-    
-    a = open ("Data.txt", "a")
-    a.write()
+    res = chatbot(str(data +"\n"+ usr))
 
+    data1 = "Me : " + usr
+    data2 = "Ai : " + res
+    
+    a.write(str(data1+"\n"))
+    a.write(str(data2+"\n"))
 
     if usr.lower() == "bye":
         break
