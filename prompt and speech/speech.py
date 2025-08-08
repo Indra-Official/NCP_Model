a3b534aa99a2d8ebaea44bda3f0f523d7520ae90
jaisai1709-afk <jaisai1709@gmail.com>
2025-08-08 08:05:18 +0530

    commit by jai

---
+++ prompt and speech/speech.py
@@ -0,0 +1,71 @@
+from elevenlabs.client import ElevenLabs
+import elevenlabs
+import playsound
+
+# Initialize ElevenLabs client
+client = ElevenLabs(
+    api_key="sk_4d10580da8584bb0c5fb72f232e55f41854101ff25c25b2b"
+)
+
+def gandhi_t2s(msg):
+    voice_id = "VR6AewLTigWG4xSOukaG" #Adam
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_monolingual_v1",
+        text=msg
+    )
+    with open("gandhi.mp3", "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
+    playsound.playsound("gandhi.mp3")
+
+def ambed_t2s(msg):
+    voice_id = "TxGEqnHWrfWFTfGW9XjX" #Josh
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_monolingual_v1",
+        text=msg
+    )
+    with open("ambed.mp3", "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
+    playsound.playsound("ambed.mp3")
+
+
+def tej_t2s(msg):
+    voice_id = "21m00Tcm4TlvDq8ikWAM" #Arnold
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_monolingual_v1",
+        text=msg
+    )
+    with open("tej.mp3", "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
+    playsound.playsound("tej.mp3")
+
+
+def mohommad_t2s(msg):
+    voice_id = "1m0aTcm4TlvDq8ikWA3N" #Sam
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_monolingual_v1",
+        text=msg
+    )
+    with open("mohommad.mp3", "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
+    playsound.playsound("mohommad.mp3")
+
+
+def ramsay_t2s(msg):
+    voice_id = "2mJ0S7gKgnOxmXk9YWAJ" #Mark
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_monolingual_v1",
+        text=msg
+    )
+    with open("ramsay.mp3", "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
+    playsound.playsound("ramsay.mp3")
